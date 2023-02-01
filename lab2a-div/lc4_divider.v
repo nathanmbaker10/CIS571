b1b2c8e41fd7bd7cfe9eee5b72f2edaec8fe392d
nathanmbaker10 <nathanmbaker@gmail.com>
2023-02-01 00:18:08 -0500

    Final Submission full points

--- lab2a-div/lc4_divider.v
+++ lab2a-div/lc4_divider.v
@@ -1,9 +1,25 @@
 /* Matthew Pearl - pearlm, Nathan Baker - nater */
 
-`include "global_modules.v"
-
 `timescale 1ns / 1ps
 `default_nettype none
+
+module mux2to1(S, A, B, Out); 
+    input wire S, A, B; 
+    output wire Out; 
+
+    assign Out = S ? B : A;
+endmodule
+
+module mux2to1_16(S, A, B, Out);
+    input wire S;
+    input wire [15:0] A, B;
+    output wire [15:0] Out;
+
+    genvar i;  
+    for (i = 0; i < 16; i = i+1) begin
+        mux2to1 m(.S(S), .A(A[i]), .B(B[i]), .Out(Out[i]));
+    end    
+endmodule
 
 module lc4_divider(input  wire [15:0] i_dividend,
                    input  wire [15:0] i_divisor,
